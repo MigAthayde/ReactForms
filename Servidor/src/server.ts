627b5b7f7bd94f59afe0bff4157669a0a50b5f4a
Miguel Carneiro <miguellcarneiro937@gmail.com>
2025-05-18 00:42:22 -0300

    backend

---
+++ Servidor/src/server.ts
@@ -0,0 +1,13 @@
+import express, { Application } from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
